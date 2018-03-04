b33db1810f83e8c2afcef54dc72a070a7931b20c
Marcelo Botega Fontana <marcelobotega@gmail.com>
2018-03-04 13:22:23 -0300

    Adicionando webhook para heroku

--- src/index.js
+++ src/index.js
@@ -2,16 +2,17 @@ require('dotenv').config();
 const moment = require('moment');
 const Telegraf = require('telegraf');
 const FB = require('fb');
+const express = require('express');
+const expressApp = express();
 
 const API_TOKEN = process.env.BOT_TOKEN || '';
-const PORT = process.env.PORT || 443;
+const PORT = process.env.PORT || 3000;
 const URL = process.env.URL || 'https://aondevaiseroalmocobot.herokuapp.com';
 const bot = new Telegraf(API_TOKEN);
 
 FB.setAccessToken(process.env.GRAPH_TOKEN);
-bot.telegram.setWebhook();
-// bot.telegram.setWebhook(`${URL}/bot${API_TOKEN}`);
-// bot.startWebhook(`/bot${API_TOKEN}`, null, PORT);
+bot.telegram.setWebhook(`${URL}/bot${API_TOKEN}`);
+expressApp.use(bot.webhookCallback(`/bot${API_TOKEN}`));
 
 bot.command('welcome', (ctx) => ctx.reply('Hello!'));
 
@@ -29,6 +30,14 @@ bot.hears(/bot angelicos/i, (ctx) => {
 bot.hears(/bot todos/i, (ctx) => ctx.reply('todos'));
 
 bot.startPolling();
+
+expressApp.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+expressApp.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
+
 
 function getAngelicosData() {
     return FB.api('/angelicosbomretiro/posts');
