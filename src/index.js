861ed5d65b826cf2733ff123dabbc3d51616e79a
Marcelo Botega Fontana <marcelobotega@gmail.com>
2018-03-03 21:46:41 -0300

    Adicionando restaurante angelicos

---
+++ src/index.js
@@ -0,0 +1,41 @@
+require('dotenv').config();
+const moment = require('moment');
+const Telegraf = require('telegraf');
+const bot = new Telegraf(process.env.BOT_TOKEN);
+const FB = require('fb');
+FB.setAccessToken(process.env.GRAPH_TOKEN);
+
+bot.telegram.setWebhook();
+
+bot.command('welcome', (ctx) => ctx.reply('Hello!'));
+
+bot.hears(/bot angelicos/i, (ctx) => {
+
+    getAngelicosData().then(res => {
+        ctx.reply(handleData(res.data));
+    }).catch(error => {
+        console.log(error);
+        ctx.reply(!error ? 'error occurred' : error.error);
+    });
+
+});
+
+bot.hears(/bot todos/i, (ctx) => ctx.reply('todos'));
+
+bot.startPolling();
+
+function getAngelicosData() {
+    return FB.api('/angelicosbomretiro/posts');
+}
+
+function handleData(data) {
+    let repeatTimes = 3;
+
+    for (let i = 0; i < repeatTimes; i++) {
+        let item = data[i];
+        if (item.message.indexOf("CARDÁPIO") > -1 && moment(item.created_time).isSame(moment(), "day")) {
+            return item.message;
+        }
+    }
+    return "Cardápio de hoje não encontrado!";
+}
