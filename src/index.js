d6a225bb368c6b9f4414724224b33e9fa2736d88
Marcelo Botega Fontana <marcelobotega@gmail.com>
2018-03-04 13:15:31 -0300

    Adicionando webhook para heroku

--- src/index.js
+++ src/index.js
@@ -9,9 +9,9 @@ const URL = process.env.URL || 'https://aondevaiseroalmocobot.herokuapp.com';
 const bot = new Telegraf(API_TOKEN);
 
 FB.setAccessToken(process.env.GRAPH_TOKEN);
-bot.telegram.setWebhook();
-bot.telegram.setWebhook(`${URL}/bot${API_TOKEN}`);
-bot.startWebhook(`/bot${API_TOKEN}`, null, PORT);
+// bot.telegram.setWebhook();
+// bot.telegram.setWebhook(`${URL}/bot${API_TOKEN}`);
+// bot.startWebhook(`/bot${API_TOKEN}`, null, PORT);
 
 bot.command('welcome', (ctx) => ctx.reply('Hello!'));
 
