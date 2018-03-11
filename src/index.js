a45e2a661ad744d3dabba2c082b4b412e31715b5
Marcelo Botega Fontana <marcelobotega@gmail.com>
2018-03-11 13:07:22 -0300

    Adicionado funcionalidade para varios restaurantes, alterar apenas o json de configuração

--- src/index.js
+++ src/index.js
@@ -9,23 +9,35 @@ const API_TOKEN = process.env.BOT_TOKEN || '';
 const PORT = process.env.PORT || 3000;
 const URL = process.env.URL || 'https://aondevaiseroalmocobot.herokuapp.com';
 const bot = new Telegraf(API_TOKEN);
-
+const restaurants = JSON.parse(process.env.RESTAURANTS);
 bot.telegram.setWebhook();
 FB.setAccessToken(process.env.GRAPH_TOKEN);
-// bot.telegram.setWebhook(`${URL}/bot${API_TOKEN}`);
-// expressApp.use(bot.webhookCallback(`/bot${API_TOKEN}`));
 
 bot.command('welcome', (ctx) => ctx.reply('Hello!'));
 
-bot.hears(/bot angelicos/i, (ctx) => {
+bot.hears(/^bot help$/i, (ctx) => {
+    let reply = "Restaurantes disponíveis:\n";
 
-    getAngelicosData().then(res => {
-        ctx.reply(handleData(res.data));
-    }).catch(error => {
-        console.log(error);
-        ctx.reply(!error ? 'error occurred' : error.error);
+    Object.keys(restaurants).forEach(function(key) {
+        let item = restaurants[key];
+        reply += item.name+" => "+item.command+"\n";
     });
 
+    ctx.reply(reply);
+});
+
+Object.keys(restaurants).forEach(function(key) {
+    let item = restaurants[key];
+    let re = new RegExp("^"+item.command+"$", "i");
+
+    bot.hears(re, (ctx) => {
+        getData(item).then(res => {
+            ctx.reply(handleData(res.data, item));
+        }).catch(error => {
+            console.log(error);
+            ctx.reply(!error ? 'error occurred' : error.error);
+        });
+    });
 });
 
 bot.hears(/bot todos/i, (ctx) => ctx.reply('todos'));
@@ -39,17 +51,20 @@ expressApp.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
 
+setInterval(function() {
+    expressApp.get(URL);
+}, 300000);
 
-function getAngelicosData() {
-    return FB.api('/angelicosbomretiro/posts');
+function getData(restaurant) {
+    return FB.api(restaurant.api);
 }
 
-function handleData(data) {
+function handleData(data, restaurant) {
     let repeatTimes = 3;
 
     for (let i = 0; i < repeatTimes; i++) {
         let item = data[i];
-        if (item.message.indexOf("CARDÁPIO") > -1 && moment(item.created_time).isSame(moment(), "day")) {
+        if (item.message.indexOf(restaurant.filter) > -1 && moment(item.created_time).isSame(moment(), "day")) {
             return item.message;
         }
     }
